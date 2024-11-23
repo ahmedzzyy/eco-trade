e8fda9e94987a4f2a3951a67efd94268be77aa82
Ahmed S. <84773790@users.noreply.github.com>
2024-11-23 04:22:31 +0000

    User Entity defined

---
+++ server/src/main/kotlin/com/ecotrade/server/user/model/entity/User.kt
@@ -0,0 +1,23 @@
+package com.ecotrade.server.user.model.entity
+
+import jakarta.persistence.*
+import jakarta.validation.constraints.Email
+
+@Entity
+@Table(name = "users")
+data class User (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(nullable = false, unique = true)
+    @Email
+    val email: String,
+
+    @Column(nullable = false)
+    val password: String, // To be encrypted later
+
+    val location: String? = null,
+
+    val profilePicture: String? = null
+)
