311a6f535cc19f28951d31ecc316e1a0ea7b8497
Ahmed S. <84773790@users.noreply.github.com>
2024-11-24 17:28:25 +0000

    Listing Controller Created

---
+++ server/src/main/kotlin/com/ecotrade/server/listing/controller/ListingController.kt
@@ -0,0 +1,64 @@
+package com.ecotrade.server.listing.controller
+
+import com.ecotrade.server.listing.dto.ListingRequest
+import com.ecotrade.server.listing.model.Listing
+import com.ecotrade.server.listing.service.ListingService
+import com.ecotrade.server.user.service.UserService
+import jakarta.persistence.EntityNotFoundException
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/listings")
+class ListingController(
+    private val listingService: ListingService,
+    private val userService: UserService
+) {
+
+    @PostMapping
+    fun createListing(@RequestBody @Valid listingRequest: ListingRequest): ResponseEntity<Void> {
+        val currentUserEmail = SecurityContextHolder.getContext().authentication.principal as String
+        val currentUser = userService.findByEmail(currentUserEmail)
+            .orElseThrow { EntityNotFoundException("User not found! Login.") }
+
+        listingService.createListing(listingRequest.toEntity(currentUser))
+
+        return ResponseEntity.noContent().build()
+    }
+
+    @GetMapping
+    fun getAllListings(
+        @RequestParam(defaultValue = "0") page: Int,
+        @RequestParam(defaultValue = "10") size: Int
+    ): ResponseEntity<List<Listing>> {
+        val listings = listingService.getAllListings() // TODO("Pagination, Sorting with ElasticSearch Later")
+        return ResponseEntity.ok(listings)
+    }
+
+    @GetMapping("/{id}")
+    fun getListingById(@PathVariable id: Long): ResponseEntity<Listing> {
+        val listing = listingService.getListingById(id)
+        return ResponseEntity.ok(listing)
+    }
+
+    @PutMapping("/{id}")
+    fun updateListingById(
+        @PathVariable id: Long,
+        @RequestBody @Valid listingRequest: ListingRequest
+    ): ResponseEntity<Void> {
+        val currentUserEmail = SecurityContextHolder.getContext().authentication.principal as String
+
+        listingService.updateListing(id, listingRequest, currentUserEmail)
+        return ResponseEntity.noContent().build()
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteListingById(@PathVariable id: Long): ResponseEntity<Void> {
+        val currentUserEmail = SecurityContextHolder.getContext().authentication.principal as String
+
+        listingService.deleteListing(id, currentUserEmail)
+        return ResponseEntity.noContent().build()
+    }
+}
