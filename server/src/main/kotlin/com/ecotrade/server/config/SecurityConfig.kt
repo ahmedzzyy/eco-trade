fc8a850f6f2f1e3b70c0a49c8af06f1917f4d424
Ahmed S. <84773790@users.noreply.github.com>
2024-11-24 00:13:10 +0000

    Security Config updated for JWT authentication

--- server/src/main/kotlin/com/ecotrade/server/config/SecurityConfig.kt
+++ server/src/main/kotlin/com/ecotrade/server/config/SecurityConfig.kt
@@ -1,14 +1,16 @@
 package com.ecotrade.server.config
 
+import com.ecotrade.server.security.JwtFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig(private val jwtFilter: JwtFilter) {
 
     @Bean
     fun passwordEncoder(): PasswordEncoder {
@@ -17,11 +19,13 @@ class SecurityConfig {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        // Temporary Spring Security allow all
         http
-            .csrf { it.disable() }
-            .authorizeHttpRequests { it.anyRequest().permitAll() }
-            .headers { it.frameOptions { it.disable() } }
+            .csrf { it.disable() } // CSRF unnecessary as stateless JWT Auth is used
+            .authorizeHttpRequests {
+                it.requestMatchers("/api/auth/**").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
 
         return http.build()
     }
